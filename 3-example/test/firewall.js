47a4b6502cf9dec05b4c4e55d88cd63b5dfc0285
Timur Sevimli <svmlitimur@gmail.com>
2023-07-12 11:50:14 +0300

    Update and optimise tests

--- 3-example/test/firewall.js
+++ 3-example/test/firewall.js
@@ -9,46 +9,46 @@ const Firewall = require('../Firewall.js');
 
 const now = Date.now();
 const exampleData = [
-  { ip: '1.1.1.1', ipv: 'ipv4', banned: true, reqCount: 3, reqTime: now },
-  { ip: '1.1.1.2', ipv: 'ipv4', banned: true, reqCount: 3, reqTime: now   },
+  { ip: '127.0.0.1', ipv: 'ipv4', banned: true, reqCount: 3, reqTime: now },
+  { ip: '127.0.0.2', ipv: 'ipv4', banned: true, reqCount: 3, reqTime: now   },
   { ip: '::1', ipv: 'ipv6', banned: true, reqCount: 3, reqTime: now },
 ];
 
 const sleep = (msec) => new Promise((resolve) => setTimeout(resolve, msec));
 
 test('Check suspicious urls', () => {
-  const fw = new Firewall({ maxReqCount: 0 });
   let i = 0;
+  const fw = new Firewall({ maxReqCount: 0 });
   for (const url of suspiciousUrls) {
-    const ip = `10.10.10.${i++}`;
-    const result = fw.interceptor({ url, ip });
+    const req = { url, ip: `10.10.0.${i++}` };
+    const result = fw.interceptor(req);
     assert.strictEqual(result, true);
   }
 });
 
 test('Non-suspicious request', () => {
-  const req = { url: '/home', ip: '127.0.0.0' };
+  const req = { url: '/home', ip: '127.0.0.1' };
   const fw = new Firewall();
   const result = fw.interceptor(req);
   assert.strictEqual(result, false);
 });
 
 test('Non-blocking request', () => {
-  const req = { url: '/home', ip: '127.0.0.0' };
+  const req = { url: '/home', ip: '127.0.0.1' };
   const fw = new Firewall({ maxReqCount: 0 });
   const result = fw.interceptor(req);
   assert.strictEqual(result, false);
 });
 
 test('Blocking request', () => {
-  const req = { url: '/admin', ip: '127.0.0.0' };
+  const req = { url: '/admin', ip: '127.0.0.1' };
   const fw = new Firewall({ maxReqCount: 0 });
   const result = fw.interceptor(req);
   assert.strictEqual(result, true);
 });
 
 test('Requests with count', () => {
-  const req = { url: '/admin', ip: '127.0.0.0' };
+  const req = { url: '/admin', ip: '127.0.0.1' };
   const fw = new Firewall({ maxReqCount: 1 });
   const res1 = fw.interceptor(req);
   const res2 = fw.interceptor(req);
@@ -57,7 +57,7 @@ test('Requests with count', () => {
 });
 
 test('More requests with count', () => {
-  const req = { url: '/admin', ip: '127.0.0.0' };
+  const req = { url: '/admin', ip: '127.0.0.1' };
   const count = 1000;
   const fw = new Firewall({ maxReqCount: count });
   for (let i = 0; i < count; i++) {
@@ -69,7 +69,7 @@ test('More requests with count', () => {
 });
 
 test('Whitelist', () => {
-  const req = { url: '/admin', ip: '1.1.1.1' };
+  const req = { url: '/admin', ip: '127.0.0.1' };
   const options = { maxReqCount: 0 };
   const fw = new Firewall(options);
   fw.addAddressToWhiteList(req.ip);
@@ -86,7 +86,7 @@ test('Validate blockList (give wrong instance)', () => {
 });
 
 test('Blocklist for IPv4', () => {
-  const req = { url: '/admin', ip: '127.0.0.0' };
+  const req = { url: '/admin', ip: '127.0.0.1' };
   const blockList = new BlockList();
   const fw = new Firewall({ maxReqCount: 1 }, blockList);
 
@@ -131,13 +131,11 @@ test('Initialization firewall without blocklist', () => {
   const fw = new Firewall({ maxReqCount: 0 });
   fw.initFirewall(exampleData);
 
-  const res1 = fw.interceptor({ url: '/home', ip: '1.1.1.1' });
-  const res2 = fw.interceptor({ url: '/home', ip: '1.1.1.2' });
-  const res3 = fw.interceptor({ url: '/home', ip: '::1' });
-
-  assert.strictEqual(res1, true);
-  assert.strictEqual(res2, true);
-  assert.strictEqual(res3, true);
+  for (const data of exampleData) {
+    const { ip } = data;
+    const res = fw.interceptor({ url: '/home', ip });
+    assert.strictEqual(res, true);
+  }
 });
 
 test('Initialization firewall with blocklist', () => {
@@ -145,33 +143,27 @@ test('Initialization firewall with blocklist', () => {
   const fw = new Firewall({ maxReqCount: 0 }, blockList);
   fw.initFirewall(exampleData);
 
-  const check1 = blockList.check('1.1.1.1');
-  const check2 = blockList.check('1.1.1.2', 'ipv4');
-  const check3 = blockList.check('::1', 'ipv6');
+  for (const data of exampleData) {
+    const { ip, ipv } = data;
 
-  assert.strictEqual(check1, true);
-  assert.strictEqual(check2, true);
-  assert.strictEqual(check3, true);
+    const check = blockList.check(ip, ipv);
+    assert.strictEqual(check, true);
 
-  const res1 = fw.interceptor({ url: '/home', ip: '1.1.1.1' });
-  const res2 = fw.interceptor({ url: '/home', ip: '1.1.1.2' });
-  const res3 = fw.interceptor({ url: '/home', ip: '::1' });
-
-  assert.strictEqual(res1, true);
-  assert.strictEqual(res2, true);
-  assert.strictEqual(res3, true);
+    const res = fw.interceptor({ url: '/home', ip });
+    assert.strictEqual(res, true);
+  }
 });
 
 test('Add address to block list', () => {
   const blockList = new BlockList();
 
-  blockList.addAddress('1.1.1.1');
-  blockList.addAddress('1.1.1.2', 'ipv4');
+  blockList.addAddress('127.0.0.1');
+  blockList.addAddress('127.0.0.2', 'ipv4');
   blockList.addAddress('::1', 'ipv6');
 
-  const res1 = blockList.rules.includes('Address: IPv4 1.1.1.1');
+  const res1 = blockList.rules.includes('Address: IPv4 127.0.0.1');
   const res2 = blockList.rules.includes('Address: IPv6 ::1');
-  const res3 = blockList.check('1.1.1.2', 'ipv4');
+  const res3 = blockList.check('127.0.0.2', 'ipv4');
 
   assert.strictEqual(res1, true);
   assert.strictEqual(res2, true);
@@ -179,7 +171,7 @@ test('Add address to block list', () => {
 });
 
 test('Non-blocking interval request', async () => {
-  const req = { url: '/admin', ip: '1.1.1.1' };
+  const req = { url: '/admin', ip: '127.0.0.1' };
   const options = { maxReqCount: 3, reqInterval: 1000 };
   const fw = new Firewall(options);
   for (let i = 0; i <= options.maxReqCount; i++) {
@@ -190,7 +182,7 @@ test('Non-blocking interval request', async () => {
 });
 
 test('Blocking interval request', async () => {
-  const req = { url: '/admin', ip: '1.1.1.1' };
+  const req = { url: '/admin', ip: '127.0.0.1' };
   const options = { maxReqCount: 1, reqInterval: 1000 };
   const fw = new Firewall(options);
   for (let i = 0; i < 3; i++) {
